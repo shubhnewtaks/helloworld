6a7d1b54f319cd65f4bffc2de9ab981453585ffb
shubhnewtaks <shubhasma@gmail.com>
2019-02-04 21:14:57 +0530

    commit2

---
+++ src/test/java/com/HelloWorldTest.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+    @Test
+    public void printName1(){
+
+    }
+
+    @Test
+    public void printName2(){
+
+    }
+}
